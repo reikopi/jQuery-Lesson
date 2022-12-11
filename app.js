ffb752e090866cd92ef25beb171aa352a5cc800c
reikopi <rk02.stmi.1018@gmail.com>
2022-12-11 04:37:53 +0000

    jQuery-Lesson_5章確認問題

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 $(function(){
-  $('.box1').hide();
+// 赤色の正方形を上から下へスライド
+  $('.box1').slideDown(function (){
+//   青色の長方形を表示
+   $('.box1').css({
+       'background-color': '#0000FF',
+        'width': '200px',
+        'height': '100px'
+// 下から上へスライド
+   }).slideUp();
+  });
 });
