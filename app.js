13c0842747b626cfc227c01ffc766150b46de3bb
reikopi <rk02.stmi.1018@gmail.com>
2022-12-14 10:12:32 +0000

    jQuery-Lesson_マウスオーバーイベント設定

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ $(function(){
 // 赤色の正方形を上から下へスライド
   $('.box1').slideDown(function (){
 //   青色の長方形を表示
-   $('.box1').css({
-       'background-color': '#0000FF',
+  $('.box1').css({
+      'background-color': '#0000FF',
         'width': '200px',
         'height': '100px'
 // 下から上へスライド
-   }).slideUp();
+  }).slideUp();
+  });
+});
+
+
+// マウスオーバーイベントの設定
+$(function(){
+  $('.box2').mouseover(function(){
+    $('.box2').css({'background-color': '#0000FF'});
   });
 });
