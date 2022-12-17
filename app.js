3a122c42a1910d1c42c950f6206000ceaa0c3ce7
reikopi <rk02.stmi.1018@gmail.com>
2022-12-17 06:58:13 +0000

    jQuery-Lesson_マウスアウトイベント_Class属性追加（Add、Remove）

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
-$(function(){
-// 赤色の正方形を上から下へスライド
-  $('.box1').slideDown(function (){
-//   青色の長方形を表示
-  $('.box1').css({
-      'background-color': '#0000FF',
-        'width': '200px',
-        'height': '100px'
-// 下から上へスライド
-  }).slideUp();
-  });
-});
+// $(function(){
+// // 赤色の正方形を上から下へスライド
+//   $('.box1').slideDown(function (){
+// //   青色の長方形を表示
+//   $('.box1').css({
+//       'background-color': '#0000FF',
+//         'width': '200px',
+//         'height': '100px'
+// // 下から上へスライド
+//   }).slideUp();
+//   });
+// });
 
 
-// マウスオーバーイベントの設定
+// // マウスオーバーイベントの設定
+// $(function(){
+//   $('.box2').mouseover(function(){
+//     $('.box2').css({'background-color': '#0000FF'});
+//   });
+//   $('.box2').mouseout(function(){
+//     $('.box2').css({'background-color':'#FF0000'});
+//   });
+// });
+
 $(function(){
-  $('.box2').mouseover(function(){
-    $('.box2').css({'background-color': '#0000FF'});
-  });
-  $('.box2').mouseout(function(){
-    $('.box2').css({'background-color':'#FF0000'});
-  });
+   $('.box2').mouseover(function(){
+    $('.box2').addClass('box2-ext');
+   });
+   $('.box2').mouseout(function(){
+    $('.box2').removeClass('box2-ext');
+   });
 });
