4278cb091a6da6bce699bfad224745f46cd9fdd7
reikopi <rk02.stmi.1018@gmail.com>
2022-12-20 10:58:09 +0000

    jQuery-Lesson_childrenを活用

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 $(function(){
-  $('.box1').on('click', function(){
-    // クリックされた要素＝thisが、スライドアップする
-    $(this).slideUp();
+  // button要素がクリックされたときに、処理を実行
+  $('button').on('click', function(){
+  // ul要素の子要素、li要素を全て取得する
+    $('ul').children().css('color','red');
   });
 });
