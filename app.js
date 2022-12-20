4f296bcaa79de5ce613ae2f27dcad252c98a1a4b
reikopi <rk02.stmi.1018@gmail.com>
2022-12-20 10:50:54 +0000

    jQuery-Lesson_slideUp全て記述

--- app.js
+++ app.js
@@ -1,43 +1,17 @@
-// $(function(){
-// // 赤色の正方形を上から下へスライド
-//   $('.box1').slideDown(function (){
-// //   青色の長方形を表示
-//   $('.box1').css({
-//       'background-color': '#0000FF',
-//         'width': '200px',
-//         'height': '100px'
-// // 下から上へスライド
-//   }).slideUp();
-//   });
-// });
-
-
-// // マウスオーバーイベントの設定
-// $(function(){
-//   $('.box2').mouseover(function(){
-//     $('.box2').css({'background-color': '#0000FF'});
-//   });
-//   $('.box2').mouseout(function(){
-//     $('.box2').css({'background-color':'#FF0000'});
-//   });
-// });
-
-// クラス属性でマウスオーバーとアウトをする
-// $(function(){
-//   $('.box2').mouseover(function(){
-//     $('.box2').addClass('box2-ext');
-//   });
-//   $('.box2').mouseout(function(){
-//     $('.box2').removeClass('box2-ext');
-//   });
-// });
-
-// マウスクリックイベントを作成
 $(function(){
-  $('.box2').on('click',function(){
-    $('.box2').addClass('box2-ext')
+  $('.bg1').on('click', function(){
+    $('.bg1').slideUp();
   });
-  $('.box2').mouseout(function(){
-    $('.box2').removeClass('box2-ext');
+
+  $('.bg2').on('click', function(){
+    $('.bg2').slideUp();
+  });
+
+  $('.bg3').on('click', function(){
+    $('.bg3').slideUp();
+  });
+
+  $('.bg4').on('click', function(){
+    $('.bg4').slideUp();
   });
 });
